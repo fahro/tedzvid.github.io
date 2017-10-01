dc81a6d95e4a4ac82195fc7c928d6767efcab70d
Harun Djulic <harun@claireit.nl>
2017-10-01 18:23:09 +0200

    t

--- js/app.js
+++ js/app.js
@@ -105,3 +105,43 @@ var goTo = function (p) {
         .$bookBlock
         .bookblock('jump', p)
 };
+
+var setLessons = function () {
+    var lessons = [
+        {
+            i: 1
+        }
+    ];
+
+    setLessonsMenu(lessons);
+    setLessonsContent(lessons);
+
+    Page.init();
+};
+
+var setLessonsMenu = function (ls) {
+    var el = document.getElementById('lessons');
+
+    for (var i = 0; i < ls.length; i++) {
+        var e = document.createElement('div');
+        e.innerHTML = '<div class="lesson n_' + ls[i].i + '" onClick="goTo(' + ls[i].i + ')">1. Damir</div>';
+        el.appendChild(e);
+    }
+}
+
+var setLessonsContent = function (ls) {
+    var b = document.getElementById('bb-bookblock');
+
+    for (var i = 1; i <= ls.length; i++) {
+        // var c = document.createElement('div'); c.classList.add('bb-item'); var
+        // leftPage = document.createElement('div'); leftPage.className =
+        // 'bb-custom-side left lsn_' + ls[i].i; leftPage.innerHTML = ls[i].i var
+        // rightPage = document.createElement('div'); rightPage.className =
+        // 'bb-custom-side right lsn_' + ls[i].i; rightPage.innerHTML = ls[i].i
+        // c.appendChild(leftPage); c.appendChild(rightPage);
+
+        $("#bb-bookblock").load('./lessons/' + i + '/template.html');
+
+        // b.appendChild(c);
+    }
+};
